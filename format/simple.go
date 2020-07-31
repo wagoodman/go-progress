1020f39e6240ee22ec33696020972aaa45bf74a1
Alex Goodman <wagoodman@gmail.com>
2020-07-31 06:55:12 -0400

    bounds check progress formatting values

--- format/simple.go
+++ format/simple.go
@@ -67,11 +67,17 @@ func NewSimpleWithTheme(width int, theme SimpleTheme, doneHexColor, todoHexColor
 
 func (s Simple) Format(p progress.Progress) (string, error) {
 	completedRatio := p.Ratio()
+	if completedRatio < 0 {
+		completedRatio = 0
+	}
 	completedCount := int(completedRatio * float64(s.width))
 	todoCount := s.width - completedCount
+	if todoCount < 0 {
+		todoCount = 0
+	}
 
-	completedSection := s.doneColor.Sprint(strings.Repeat(string(s.charSet[fullPosition]), completedCount))
-	todoSection := s.todoColor.Sprint(strings.Repeat(string(s.charSet[fullPosition]), todoCount))
+	completedSection := s.doneColor.Sprint(strings.Repeat(s.charSet[fullPosition], completedCount))
+	todoSection := s.todoColor.Sprint(strings.Repeat(s.charSet[fullPosition], todoCount))
 
 	return s.charSet[leftCapPosition] + completedSection + todoSection + s.charSet[rightCapPosition], nil
 }
