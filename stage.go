699ab1ce22db0def0b0694b04893d8698d51ec96
Will Murphy <will.murphy@anchore.com>
2023-09-08 07:49:47 -0400

    Expose atomic update to current stage
    The stager is often used in different go routines. The lack of an atomic
    setter was forcing clients to either manager their own locks or accept a
    minor race condition.
    
    Signed-off-by: Will Murphy <will.murphy@anchore.com>

--- stage.go
+++ stage.go
@@ -1,15 +1,40 @@
 package progress
 
+import "sync/atomic"
+
 type Stager interface {
 	Stage() string
 }
 
 type Stage struct {
-	Current string
+	currentStage atomic.Value
 }
 
-func (s Stage) Stage() string {
-	return s.Current
+func (s *Stage) getCurrentStage() string {
+	return s.currentStage.Load().(string)
+}
+
+func (s *Stage) setCurrentStage(newStage string) {
+	s.currentStage.Store(newStage)
+}
+
+func (s *Stage) Stage() string {
+	return s.getCurrentStage()
+}
+
+func (s *Stage) Set(newStage string) {
+	s.setCurrentStage(newStage)
+}
+
+func NewStage(current string) *Stage {
+	result := Stage{}
+	result.currentStage.Store(current)
+	return &result
+}
+
+type StagerSettable interface {
+	Stager
+	Set(newStage string)
 }
 
 type StagedMonitorable interface {
